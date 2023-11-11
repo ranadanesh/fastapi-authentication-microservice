417c3cd8a06737114411510b30dc739c8de15e48
ranadanesh <ranadanesh.79@gmail.com>
2023-11-11 09:05:29 +0330

    feat: updated jwt

--- auth/jwt.py
+++ auth/jwt.py
@@ -4,16 +4,16 @@ import uuid
 from dotenv import dotenv_values
 
 
-def create_access_token(username):
+def create_access_token(email):
     jti = uuid.uuid4().hex
     return jwt.encode(
         {
-            "username": username,
+            "email": email,
             "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=15),
             "iat": datetime.datetime.utcnow(),
             "jti": jti
         },
-        key=None, algorithm="HS256")
+        'secret', algorithm="HS256")
 
 
 def decode_access_token(token):
@@ -21,16 +21,16 @@ def decode_access_token(token):
     return payload
 
 
-def create_refresh_token(username):
+def create_refresh_token(email):
     jti = uuid.uuid4().hex
     return jwt.encode(
         {
-            "username": username,
+            "email": email,
             "exp": datetime.datetime.utcnow() + datetime.timedelta(days=10),
             "iat": datetime.datetime.utcnow(),
             "jti": jti
         },
-        key=None, algorithm="HS256")
+        'secret', algorithm="HS256")
 
 
 def decode_refresh_token(token):
